e19d11f139cdf9faf9825f583ff41c271c57b1b2
HuangKuoChen <jimcorn615@gmail.com>
2018-10-10 20:47:00 +0900

    Add meta

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
 <title>裕德食品企業(Yu-De)─裕德製麵廠，專業生鮮麵製造</title>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta name="description" content="裕德食品企業，高雄市路竹區中山路1293巷42號，電話：(07)696-6089。裕德高雄製麵廠，專業生鮮麵製造，生麵、熟麵、冷凍麵、牛排麵、鐵板麵、烏龍麵、蕎麥麵、日式拉麵、刀削麵、涼麵、粄條、調味包。連鎖餐飲、牛排館、學校團膳、機關團體、餐飲美食、商店夜市，歡迎洽購。">
+	<meta name="keywords" content="裕德,裕德食品企業,製麵,高雄製麵,台南製麵,南部製麵,製麵廠,裕德製麵廠,專業生鮮麵製造,生麵,熟麵,冷凍麵,牛排麵,鐵板麵,烏龍麵,蕎麥麵,陽春麵,鹽水意麵,油麵,米苔目,拉麵,日式拉麵,刀削麵,涼麵,粄條,麵疙瘩,水餃,水餃皮,餛飩,餛飩皮,調味包,連鎖餐飲,牛排館,學校團膳,機關團體,餐飲美食" />
 	<link rel="stylesheet" type="text/css" href="css/slick.css"/>
 	<link rel="stylesheet" type="text/css" href="css/base.css">
 	<link rel="icon" href="img/bit.png" type="image/x-icon">
@@ -13,11 +15,11 @@
 	<script type="text/javascript" src="scripts/base.js"></script>
 	<div id="header_menu">
 		<div id="menu">
-			<a id="about_btn" class="menu_btn" href="about.php"><img src="img/menu/menu_about.png"/></a>
-			<a id="certificate_btn" class="menu_btn" href="certificate.php"><img src="img/menu/menu_certificate.png"/></a>
-			<a id="products_btn" class="menu_btn" href="products.php"><img src="img/menu/menu_products.png"/></a>
-			<a id="report_btn" class="menu_btn" href="report.php"><img src="img/menu/menu_report.png"/></a>
-			<a id="company_btn" class="menu_btn" href="company.php"><img src="img/menu/menu_contact.png"/></a>
+			<a id="about_btn" class="menu_btn" href="about"><img src="img/menu/menu_about.png"/></a>
+			<a id="certificate_btn" class="menu_btn" href="certificate"><img src="img/menu/menu_certificate.png"/></a>
+			<a id="products_btn" class="menu_btn" href="products"><img src="img/menu/menu_products.png"/></a>
+			<a id="report_btn" class="menu_btn" href="report"><img src="img/menu/menu_report.png"/></a>
+			<a id="company_btn" class="menu_btn" href="company"><img src="img/menu/menu_contact.png"/></a>
 		</div>
 	</div>
 	<div id="pagetop">
@@ -27,7 +29,7 @@
 
 <body id="top">
 	<div id="header">
-		<a href="index.php">
+		<a href="index">
 			<img id="logo" src="img/logo.png" alt="裕德">
 		</a>
 		<img id="phone" src="img/phone.png">
@@ -42,6 +44,6 @@
 		<div id="icon">
 			<a href="https://www.facebook.com/yude.coltd" target="_blank"><img class="icon_img" src="img/icon_fb.png"></a>
 			<a href="http://line.me/ti/p/fHAKi77xVF" target="_blank"><img class="icon_img" src="img/icon_line.png"></a>
-			<a href="contact.php"><img class="icon_img" src="img/icon_mail.png"></a>
+			<a href="contact"><img class="icon_img" src="img/icon_mail.png"></a>
 		</div>
 	</div>
